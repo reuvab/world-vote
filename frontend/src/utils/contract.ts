dc749adde0f69cea351008d1dc41530a3508ba90
Reuven Aboulker <reuven.aboulker@gmail.com>
2023-05-13 22:29:38 +0100

    update contract address

--- frontend/src/utils/contract.ts
+++ frontend/src/utils/contract.ts
@@ -1,7 +1,7 @@
 import { ethers } from 'ethers';
 import { ContractTransaction } from 'ethers';
 
-const CONTRACT_ADDRESS = '0x4aBCa6D51b42bA971919056349C63e1da0bbC765';
+const CONTRACT_ADDRESS = '0x4673aE3D40bb39A6287C463Eab0CeeA167113B7B';
 
 export function getProvider() {
   return new ethers.providers.Web3Provider((window as any).ethereum, 80001);
