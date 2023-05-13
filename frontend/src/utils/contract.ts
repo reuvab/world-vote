4b9a0ed166518e5c439a947e0e1424b3fad753c9
Reuven Aboulker <reuven.aboulker@gmail.com>
2023-05-13 23:51:18 +0100

    update smart contract

--- frontend/src/utils/contract.ts
+++ frontend/src/utils/contract.ts
@@ -1,7 +1,7 @@
 import { ethers } from 'ethers';
 import { ContractTransaction } from 'ethers';
 
-const CONTRACT_ADDRESS = '0x274B73F17eeED99bc0cdf0521F55AEc84FE2b6CE';
+const CONTRACT_ADDRESS = '0xe43d0aC9f6cEAD387581C4e294bdD5e8Fd0f5B95';
 
 export function getProvider() {
   return new ethers.providers.Web3Provider((window as any).ethereum, 80001);
