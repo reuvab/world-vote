013c04d4d442128bbe296ada5df4bda5eb7a2e46
Reuven Aboulker <reuven.aboulker@gmail.com>
2023-05-14 01:24:08 +0100

    add homepage

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,7 +2,8 @@
 import Image from 'next/image';
 import Button from '@mui/base/Button';
 import { useRouter } from 'next/navigation';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { getContract } from '@/utils/contract';
 
 function shortenWalletAddress(walletAddress: string, length: number): string {
   const prefixLength = Math.floor(length / 2);
@@ -22,6 +23,18 @@ export default function Home() {
         <p>We are the best to do stuff and change the world</p>
         <Button className='button-secondary' onClick={() => router.push('/create')}>
           Create Survey
+        </Button>
+
+        <Button className='button' onClick={() => router.push('/view/1')}>
+          View Survey Renewable Energy
+        </Button>
+
+        <Button className='button' onClick={() => router.push('/view/2')}>
+          View Survey Carbon Tax
+        </Button>
+
+        <Button className='button' onClick={() => router.push('/view/3')}>
+          View Survey Climate Science
         </Button>
       </div>
     </main>
